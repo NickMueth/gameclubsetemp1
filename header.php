e6374e4a2348126994a384981d5b150e9e6b42f1
NickMueth <musicman2797@gmail.com>
2016-02-03 15:26:13 -0600

    Create header.php

---
+++ header.php
@@ -0,0 +1,64 @@
+<html>
+<head>
+    <title>SEMOCSCLUB</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="row" style="margin-left: 0px; margin-right: 0px">
+        <div id="main-title-area" class="row container-fluid" style="margin-left: 0px; margin-right: 0px">
+            <div class="col-md-12">
+                <nav class="navbar navbar-fixed-top navbar-inverse">
+                    <div class="container-fluid">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <a class="navbar-brand" href="/">
+                                <img alt="Brand" src="./images/site/42-512.png" height="40" width="40" style="margin-top: -10px" />
+                            </a>
+                        </div>
+
+                        <!-- Collect the nav links, forms, and other content for toggling -->
+                        <div class="collapse navbar-collapse" id="navbar-collapse-1">
+                            <ul class="nav navbar-nav">
+                                <li><a href="./">Home</a></li>
+                                <li><a href="http://hackathon.semocsclub.net" target="_blank">Hackathon</a></li>
+                                <li><a href="usefulLinks.php">Useful Links</a></li>
+                            </ul>
+                            <ul class="nav navbar-nav navbar-right">
+                            </ul>
+
+                        </div>
+                        <!-- /.navbar-collapse -->
+                    </div>
+                    <!-- /.container-fluid -->
+                </nav>
+
+                <div class="page-header" style="padding-top: 20px">
+                    <h1>Game Club SE Temp Website<small>beep boop oop words</small></h1>
+                </div>
+            </div>
+        </div>
+
+        <div id="main-content-area" class="row container-fluid" style="margin-left: 0px; margin-right: 0px">
+
+            <div id="main-left-column" class="col-md-2 hidden-sm hidden-xs">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Menu</div>
+                    <div class="panel-body">
+                        <ul class="list-group">
+                            <li class="list-group-item list-group-item-success"><a href="/">Home</a></li>
+                            <li class="list-group-item list-group-item-info"><a href="gameclubse.tk:8123" target="_blank">Dynamic Map</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <!-- end main-left-column -->
+
+            <div id="main-center-content" class="col-md-8 well">
