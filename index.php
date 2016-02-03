2f52f119f26cbca279f6f6d340c9119f19d4fc5f
NickMueth <musicman2797@gmail.com>
2016-02-03 15:21:24 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-<?php include_one("home.html"); ?>
+<?php
+include('header.php');
+?>
+
+	<div>
+		<p>I know its not much, but I did it in 5(ish) minutes!</p>
+		<p>We're going to be working on something more proper, so hopefully that will be up sooner than later.</p>
+	</div>
+	<div>
+		<h3>Sign up links here!</h3>
+    <p>Hackathon 1 - Fall 2015 - Sep 26th and 27th</p>
+    <a href="http://hackathon.semocsclub.net" alt="http://goo.gl/forms/iQ3OxODrkv">Hackathon 2 - Fall 2015 - Nov 7th and 8th</a>
+    <p></p>
+    <a href="https://megaminerai.com" target="_blank">Megaminer AI 16 - Anarchy - Nov 14 and 15th</a>
+	</div>
+
+<?php
+include('footer.php');
+?>
