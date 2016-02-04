d2f8b5d0741704839f3ed8c26e641a39f806c51c
NickMueth <musicman2797@gmail.com>
2016-02-03 23:27:24 -0600

    Create dynmap.php

---
+++ dynmap.php
@@ -0,0 +1,9 @@
+<?php
+include('header.php');
+?>
+      <h3>Dynamic Map</h3>
+      <div class="embed-responsive embed-responsive-16by9">
+        <iframe class="embed-responsive-item" src="..."></iframe>
+      </div>
+      
+      
