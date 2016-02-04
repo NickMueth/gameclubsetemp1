e2e61e4a4043b277b66717771009a230b0087f46
NickMueth <musicman2797@gmail.com>
2016-02-03 23:27:51 -0600

    Update dynmap.php

--- dynmap.php
+++ dynmap.php
@@ -3,7 +3,7 @@ include('header.php');
 ?>
       <h3>Dynamic Map</h3>
       <div class="embed-responsive embed-responsive-16by9">
-        <iframe class="embed-responsive-item" src="..."></iframe>
+        <iframe class="embed-responsive-item" src="96.40.206.189:8123"></iframe>
       </div>
       
       
